0f0684ca144a567d18366c97f5275af234f1c371
Michael Aldridge <aldridge.mac@gmail.com>
2015-05-23 18:22:00 -0500

    Add Ubuntu Preseed and firstBoot files to preseed_site

---
+++ roles/preseed_site/files/preseed/firstBootClient.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script runs the first boot install tasks on the hardware
+
+# Install the proprietary gfx installers, if any
+apt-get install -y ubuntu-drivers-common
+ubuntu-drivers autoinstall
+
+# Be absolutely sure ansible is installed
+apt-get install -y python-dev python-pip
+pip install ansible
+
+# Attempt to run the main ansible installer
+ansible-pull -U https://github.com/phy1729/cv_config_bleeding.git
+
+# Reboot so that everything starts up cleanly
+shutdown -r now
