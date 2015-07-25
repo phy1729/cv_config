9264ea263c387cddf29c610228b1a55247db6126
Michael Aldridge <aldridge.mac@gmail.com>
2015-07-25 02:37:56 -0500

    Replace account_site

--- roles/account_site/templates/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ldap_admin_username = '{{ account_admin_username | replace("\\", "\\\\") | replace("'", "\'") }}';
-$ldap_admin_password = '{{ account_admin_password | replace("\\", "\\\\") | replace("'", "\'") }}';
-$base_dn = 'OU=Lounge Users, DC=ad, DC=collegiumv, DC=org';
-$config_salt = '{{ account_salt | replace("\\", "\\\\") | replace("'", "\'") }}';
-?>
