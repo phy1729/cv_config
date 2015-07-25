396c90d6ec5941082c34e8b8a292a2bf0eb8bd1c
Michael Aldridge <aldridge.mac@gmail.com>
2015-07-25 15:04:22 -0500

    Deployment Hotfixes
    This commit does the following actions:
    - Install fglrx as the preferred graphics driver
    - Add CVBackground splash during install process
    - Fix account_site firewall rules to allow KDC communications
    - Add printers to clients
    - Fix an NFS lock interaction issue that made clients unusably slow
    - Update Account site submodule
      - Now sends out usernames with passwords
      - Included a message that reminds users of caps sensitivity
    - Update Client module
      - Configure xscreensaver defaults (fiberlamp)
      - Correct the ansible virtual package loop issue
      - Remove the Grub2 Recovery Mode option
      - Update the default config for awesome
      - Install additional packages available for awesome
      - Install the shutter screenshot program

--- roles/preseed_site/files/preseed/firstBootClient.sh
+++ roles/preseed_site/files/preseed/firstBootClient.sh
@@ -2,13 +2,16 @@
 
 # This script runs the first boot install tasks on the hardware
 
-# Install the proprietary gfx installers, if any
-apt-get install -y ubuntu-drivers-common
-ubuntu-drivers autoinstall
+# Install the proprietary gfx installers
+apt-get install -y fglrx
 
-# Be absolutely sure ansible is installed
-apt-get install -y python-dev python-pip git
+# Be absolutely sure deployment software is installed
+apt-get install -y python-dev python-pip git fbi
 pip install ansible
+
+# Show the installer splash
+curl -o /tmp/background.bmp http://preseed.collegiumv.org/CVBackground5.bmp
+fbi -d /dev/fb0 -T 1 -a /tmp/background.bmp
 
 # Attempt to run the main ansible installer
 ansible-pull -U https://github.com/collegiumv/cv_config.git
