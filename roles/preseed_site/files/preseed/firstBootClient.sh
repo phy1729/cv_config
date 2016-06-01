27a527c5f33f601f5e3a361f77b05283b3f0ed41
Michael Aldridge <aldridge.mac@gmail.com>
2016-06-01 12:11:18 -0500

    Update preseed_site
    Use Void assets and config files instead of ubuntu ones.

--- roles/preseed_site/files/preseed/firstBootClient.sh
+++ roles/preseed_site/files/preseed/firstBootClient.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
+# Wait until we can talk to the repo
+while ! ping -c1 repo.collegiumv.org &>/dev/null; do :; done
+
 # This script runs the first boot install tasks on the hardware
-
-# Install the proprietary gfx installers
-apt-get install ubuntu-drivers-common
-
-UBUNTUDRIVERS=`ubuntu-drivers list`
-
-if [[ $UBUNTUDRIVERS == *"fglrx"* ]]
-then
-    apt-get install -y fglrx
-fi
-
-if [[ $UBUNTUDRIVERS == *"nvidia"* ]]
-then
-    apt-get install -y nvidia-340 nvidia-331
-fi
-
-# Be absolutely sure deployment software is installed
-apt-get install -y python-dev python-pip git fbi
-pip install ansible
-
-# Show the installer splash
-curl -o /tmp/background.bmp http://preseed.collegiumv.org/CVBackground5.bmp
-fbi -d /dev/fb0 -T 1 -a /tmp/background.bmp
+xbps-install -R http://repo.collegiumv.org/current -Syu
+xbps-install -R http://repo.collegiumv.org/current -Syu
+xbps-install -R http://repo.collegiumv.org/current -y curl fbv ansible git-all
 
 # Attempt to run the main ansible installer
 ansible-pull -U https://github.com/collegiumv/cv_config.git
+
+# Remove the firstboot script
+rm -rf /etc/sv/firstboot
+rm -rf /var/service/firstboot
 
 # Reboot so that everything starts up cleanly
 shutdown -r now
